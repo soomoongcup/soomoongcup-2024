b672e854c251b7196bef179d62c96be56dcbe856
Hepheir <hepheir@gmail.com>
2024-05-16 02:44:52 +0900

    feat: 피보나치 O(\log N) 풀이 소스 추가

---
+++ 예비 소집/피보나치/fast_fibo.py
@@ -0,0 +1,33 @@
+FIBO_MAT = [
+    [1,1],
+    [1,0],
+]
+
+
+def matpow(mat: list[list[int]], exp: int):
+    if exp != 1:
+        mat = matpow(mat, exp//2)
+        mat = matmul(mat, mat)
+        if exp % 2 == 1:
+            mat = matmul(mat, FIBO_MAT)
+    return mat
+
+
+def matmul(m1: list[list[int]], m2: list[list[int]]):
+    return [
+        [m1[0][0]*m2[0][0] + m1[0][1]*m2[1][0], m1[0][0]*m2[0][1] + m1[0][1]*m2[1][1]],
+        [m1[1][0]*m2[0][0] + m1[1][1]*m2[1][0], m1[1][0]*m2[0][1] + m1[1][1]*m2[1][1]],
+    ]
+
+
+def fib(n: int) -> int:
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    mat = [
+        [1,1],
+        [1,0],
+    ]
+    mat = matpow(mat, n-1)
+    return mat[0][0]
