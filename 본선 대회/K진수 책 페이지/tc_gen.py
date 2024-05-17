30135b4ad0fa31b3c05756c540cad0469855d1f2
Hepheir <hepheir@gmail.com>
2024-05-17 16:09:43 +0900

    fix: 테스트케이스 생성조건 오류 수정

--- 본선 대회/K진수 책 페이지/tc_gen.py
+++ 본선 대회/K진수 책 페이지/tc_gen.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import List
+import math
 
 from oj import Problem
 from oj import validators
@@ -19,6 +20,8 @@ ZIP_NAME = PROBLEM_TITLE+'.zip'
 
 n_validator = validators.RangeValidator(lo=1, hi=int(1e9))
 k_validator = validators.RangeValidator(lo=2, hi=16)
+
+big_o_converage_validator = validators.RangeValidator(hi=int(5e7))
 
 
 def validate_default_limitations(T: int, N: List[int], K: List[int], OUTPUT: List[str]) -> bool:
@@ -41,7 +44,7 @@ if __name__ == '__main__':
     p = Problem(PROBLEM_TITLE)
 
     """
-    테스트케이스 1
+    테스트케이스 1 (배점 20점)
 
     int 만 사용해도 풀 수 있는 테스트케이스.
     모든 숫자를 하나하나 전부 세어도 풀 수 있는 문제.
@@ -49,15 +52,22 @@ if __name__ == '__main__':
     10진수만 다룬다.
 
     O(N \log_K N)
+    * \sum N = 약 5천만 이하
     """
-    TESTCASE_ID = '1'
-    N = [1, 7, 10, 11, 19, 333, 500, 957, 999, 1000, 98765, 111111, 1234567, 9999999, 152939251, 543212345, 1000000000]
-    K = [10] * len(N)
+    testcase_id = '1'
+    shared_inputs = [1, 7, 10, 11, 19, 333, 500, 957, 999, 1000, 98765, 111111, 1234567, 5292751]
+    bases = [10]
+
+    N = shared_inputs * len(bases)
+    K = []
+    for base in bases:
+        K.extend([base] * len(shared_inputs))
     T = len(N)
     OUTPUT = [solve(n, k) for n, k in zip(N, K)]
 
     validate_default_limitations(T, N, K, OUTPUT)
 
+    assert big_o_converage_validator.validate(sum(N) * math.log10(sum(N)))
     assert validators.int32_converage_validator.validate_all(N)
     assert validators.int32_converage_validator.validate_all(K)
     for i in range(T):
@@ -65,12 +75,12 @@ if __name__ == '__main__':
         assert len(output) == K[i]
         assert validators.int32_converage_validator.validate_all(output)
 
-    create_testcase(p, TESTCASE_ID, T, N, K)
+    create_testcase(p, testcase_id, T, N, K)
 
 
 
     """
-    테스트케이스 2
+    테스트케이스 2 (배점 30점)
 
     int 만 사용해도 풀 수 있는 테스트케이스.
     모든 숫자를 하나하나 전부 세어도 풀 수 있는 문제.
@@ -80,17 +90,20 @@ if __name__ == '__main__':
     O(N \log_K N)
     * \sum N = 약 5천만 이하
     """
-    TESTCASE_ID = '2'
-    N = [1, 7, 10, 11, 19, 333, 500, 957, 999, 1000, 98765, 111111, 1234567, 4599999] * 15
+    testcase_id = '2'
+    shared_inputs = [1, 7, 10, 11, 19, 333, 500, 957, 999, 1000, 98765]
+    bases = [*range(2, 17)]
+
+    N = shared_inputs * len(bases)
     K = []
-    for base in range(2, 17):
-        K.extend([base] * (len(N)//15))
+    for base in bases:
+        K.extend([base] * len(shared_inputs))
     T = len(N)
     OUTPUT = [solve(n, k) for n, k in zip(N, K)]
 
     validate_default_limitations(T, N, K, OUTPUT)
 
-    assert validators.RangeValidator(hi=int(5e8)).validate_one(sum(N)), f'{sum(N)} should be less than {int(5e8)}'
+    assert big_o_converage_validator.validate(sum(N) * math.log(sum(N), min(K)))
     assert validators.int32_converage_validator.validate_all(N)
     assert validators.int32_converage_validator.validate_all(K)
     for i in range(T):
@@ -98,12 +111,12 @@ if __name__ == '__main__':
         assert len(output) == K[i]
         assert validators.int32_converage_validator.validate_all(output)
 
-    create_testcase(p, TESTCASE_ID, T, N, K)
+    create_testcase(p, testcase_id, T, N, K)
 
 
 
     """
-    테스트케이스 3
+    테스트케이스 3 (배점 50점)
 
     long long 은 사용해야 풀 수 있는 테스트케이스.
     모든 숫자를 하나하나 전부 세어서는 풀 수 없는 문제.
@@ -113,11 +126,14 @@ if __name__ == '__main__':
 
     O(K \log_K N)
     """
-    TESTCASE_ID = '3'
-    N = [1, 7, 10, 11, 19, 333, 500, 957, 999, 1000, 98765, 111111, 1234567, 9999999, 152939251, 987656789, 1000000000] * 15
+    testcase_id = '3'
+    shared_inputs = [1, 7, 10, 11, 19, 333, 500, 957, 999, 1000, 98765, 111111, 1234567, 9999999, 152939251, 543212345, 987656789, 1000000000]
+    bases = [*range(2, 17)]
+
+    N = shared_inputs * len(bases)
     K = []
-    for base in range(2, 17):
-        K.extend([base] * (len(N)//15))
+    for base in bases:
+        K.extend([base] * len(shared_inputs))
     T = len(N)
     OUTPUT = [solve(n, k) for n, k in zip(N, K)]
 
@@ -133,7 +149,7 @@ if __name__ == '__main__':
         all_outputs.extend(output)
     assert validators.RangeValidator(lo=constants.UINT32_MAX_VALUE).validate_any(all_outputs), f"{max(all_outputs)} is less than {constants.UINT32_MAX_VALUE}"
 
-    create_testcase(p, TESTCASE_ID, T, N, K)
+    create_testcase(p, testcase_id, T, N, K)
 
 
     print(f'{len(p.testcases)} testcases generated.')
